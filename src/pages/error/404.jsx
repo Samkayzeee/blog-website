6af5196878c6bcc9c29ad65ad08752eed6da5ed8
Samkayzeee <lasisiabdulsamad9@gmail.com>
2023-02-05 17:12:35 +0100

    added 404 page

--- src/pages/error/404.jsx
+++ src/pages/error/404.jsx
@@ -1,7 +1,19 @@
+import { Link } from 'react-router-dom';
+import './404.css';
+
+
 const Error404Page = () => {
     return ( 
         <>
-            Page Not Found
+            <div className="error_404">
+                <h1>Oops!</h1>
+                <h3>404 - PAGE NOT FOUND</h3>
+                <p>
+                    The page you are looking for might have been removed 
+                    had it's name changed or is temporarily unavailable.
+                </p>
+                <Link to={'/'}>GO TO HOMEPAGE</Link>
+            </div>
         </>
      );
 }
